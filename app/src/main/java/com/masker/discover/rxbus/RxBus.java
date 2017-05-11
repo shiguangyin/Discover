40299144d6be03499a4fa8cf59d589c658ad63a2
shiguangyin <shiguangyin928@qq.com>
2017-05-12 00:01:35 +0800

    add rxbus

---
+++ app/src/main/java/com/masker/discover/rxbus/RxBus.java
@@ -0,0 +1,54 @@
+package com.masker.discover.rxbus;
+
+import android.text.style.TtsSpan;
+
+import rx.Observable;
+import rx.subjects.PublishSubject;
+import rx.subjects.SerializedSubject;
+import rx.subjects.Subject;
+
+/**
+ * CreatedBy: masker
+ * Date: 2017/5/11
+ * Description:
+ */
+
+public class RxBus {
+    private final Subject<Object,Object> mBus;
+    private static volatile RxBus sInstance;
+
+    private RxBus(){
+        mBus = new SerializedSubject<>(PublishSubject.create());
+    }
+
+
+    public static RxBus getInstance(){
+        if(sInstance == null){
+            synchronized (RxBus.class){
+                if(sInstance == null){
+                    sInstance = new RxBus();
+                }
+            }
+        }
+        return sInstance;
+    }
+
+    /**
+     * send event
+     * @param event
+     */
+    public static void post(Object event){
+        getInstance().mBus.onNext(event);
+    }
+
+    /**
+     * receive event
+     * @param klass
+     * @param <T>
+     * @return
+     */
+    public static <T> Observable<T> toObservable(Class<T> klass){
+        return getInstance().mBus.ofType(klass);
+    }
+
+}
