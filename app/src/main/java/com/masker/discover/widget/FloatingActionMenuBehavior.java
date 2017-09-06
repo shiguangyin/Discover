efc12cf44506bfa0295955683841e48a76cb5f41
shiguangyin <shiguangyin928@gmail.com>
2017-09-06 10:46:52 +0800

    refactor fab behavior

--- app/src/main/java/com/masker/discover/widget/FloatingActionMenuBehavior.java
+++ app/src/main/java/com/masker/discover/widget/FloatingActionMenuBehavior.java
@@ -1,21 +1,18 @@
 package com.masker.discover.widget;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.content.Context;
-import android.os.Build;
-import android.os.Handler;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.Snackbar;
 import android.support.v4.view.ViewCompat;
-import android.support.v4.view.ViewPropertyAnimatorListener;
 import android.support.v4.view.animation.FastOutSlowInInterpolator;
 import android.util.AttributeSet;
 import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
 import android.view.animation.Interpolator;
 
 import com.github.clans.fab.FloatingActionMenu;
-import com.masker.discover.R;
+import com.orhanobut.logger.Logger;
 
 /**
  * Author: masker
@@ -29,6 +26,7 @@ public class FloatingActionMenuBehavior extends CoordinatorLayout.Behavior<Float
     private boolean mIsAnimatingIn = false;
 
     public FloatingActionMenuBehavior(Context context, AttributeSet attrs) {
+        super(context,attrs);
     }
 
     @Override
@@ -46,6 +44,7 @@ public class FloatingActionMenuBehavior extends CoordinatorLayout.Behavior<Float
     @Override
     public boolean onStartNestedScroll(CoordinatorLayout coordinatorLayout,
                                        FloatingActionMenu child, View directTargetChild, View target, int nestedScrollAxes) {
+        Logger.i("==! "+(nestedScrollAxes == ViewCompat.SCROLL_AXIS_VERTICAL));
         return nestedScrollAxes == ViewCompat.SCROLL_AXIS_VERTICAL ||
                 super.onStartNestedScroll(coordinatorLayout, child, directTargetChild, target,
                         nestedScrollAxes);
@@ -56,97 +55,62 @@ public class FloatingActionMenuBehavior extends CoordinatorLayout.Behavior<Float
                                View target, int dxConsumed, int dyConsumed, int dxUnconsumed, int dyUnconsumed) {
         super.onNestedScroll(coordinatorLayout, child, target, dxConsumed, dyConsumed, dxUnconsumed,
                 dyUnconsumed);
-
+        Logger.i("dxConsumed = "+dxConsumed+"  ; dyConsumed = "+dyConsumed);
         if (dyConsumed > 10 && !this.mIsAnimatingOut && !this.mIsAnimatingIn && child.getVisibility() == View.VISIBLE) {
             // User scrolled down and the FAB is currently visible -> hide the FAB
             if (child.isOpened()) {
                 child.close(true);
-                new Handler().postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        animateOut(child);
-                    }
-                }, 200);
             }
-            else {
-                animateOut(child);
-            }
+            animateOut(child);
         } else if (dyConsumed < -10 && !this.mIsAnimatingOut && !this.mIsAnimatingIn && child.getVisibility() != View.VISIBLE) {
             // User scrolled up and the FAB is currently not visible -> show the FAB
             animateIn(child);
         }
     }
 
-    // Same animation that FloatingActionButton.Behavior uses to hide the FAB when the AppBarLayout exits
-    private void animateOut(final FloatingActionMenu button) {
-        if (Build.VERSION.SDK_INT >= 14) {
-            ViewCompat.animate(button).translationYBy(200F).alpha(0.0F).setInterpolator(INTERPOLATOR).withLayer()
-                    .setListener(new ViewPropertyAnimatorListener() {
-                        public void onAnimationStart(View view) {
-                            FloatingActionMenuBehavior.this.mIsAnimatingOut = true;
-                        }
+    private void animateOut(final FloatingActionMenu menu) {
+        menu.animate().translationYBy(200).withLayer()
+                .setListener(new AnimatorListenerAdapter() {
 
-                        public void onAnimationCancel(View view) {
-                            FloatingActionMenuBehavior.this.mIsAnimatingOut = false;
-                        }
+                    @Override
+                    public void onAnimationStart(Animator animation) {
+                        mIsAnimatingOut = true;
+                    }
 
-                        public void onAnimationEnd(View view) {
-                            FloatingActionMenuBehavior.this.mIsAnimatingOut = false;
-                            view.setVisibility(View.GONE);
-                        }
-                    }).start();
-        } else {
-            Animation anim = AnimationUtils.loadAnimation(button.getContext(), R.anim.design_fab_out);
-            anim.setInterpolator(INTERPOLATOR);
-            anim.setDuration(200L);
-            anim.setAnimationListener(new Animation.AnimationListener() {
-                public void onAnimationStart(Animation animation) {
-                    FloatingActionMenuBehavior.this.mIsAnimatingOut = true;
-                }
+                    @Override
+                    public void onAnimationCancel(Animator animation) {
+                        mIsAnimatingOut = false;
+                    }
 
-                public void onAnimationEnd(Animation animation) {
-                    FloatingActionMenuBehavior.this.mIsAnimatingOut = false;
-                    button.setVisibility(View.GONE);
-                }
-
-                @Override
-                public void onAnimationRepeat(final Animation animation) {
-                }
-            });
-            button.startAnimation(anim);
-        }
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        mIsAnimatingOut = false;
+                        menu.setVisibility(View.INVISIBLE);
+                    }
+                });
     }
 
-    // Same animation that FloatingActionButton.Behavior uses to show the FAB when the AppBarLayout enters
-    private void animateIn(FloatingActionMenu button) {
-        button.setVisibility(View.VISIBLE);
-        if (Build.VERSION.SDK_INT >= 14) {
-            ViewCompat.animate(button).translationYBy(-200F).alpha(1.0F)
-                    .setInterpolator(INTERPOLATOR).withLayer().setListener(new ViewPropertyAnimatorListener() {
-                @Override
-                public void onAnimationStart(View view) {
-                    FloatingActionMenuBehavior.this.mIsAnimatingIn = true;
+    private void animateIn(FloatingActionMenu menu) {
+        menu.setVisibility(View.VISIBLE);
+        menu.animate().translationYBy(-200).withLayer()
+                .setListener(new AnimatorListenerAdapter() {
 
-                }
+                    @Override
+                    public void onAnimationStart(Animator animation) {
+                        mIsAnimatingIn = true;
+                    }
 
-                @Override
-                public void onAnimationEnd(View view) {
-                    FloatingActionMenuBehavior.this.mIsAnimatingIn = false;
+                    @Override
+                    public void onAnimationCancel(Animator animation) {
+                        mIsAnimatingIn = false;
+                    }
 
-                }
 
-                @Override
-                public void onAnimationCancel(View view) {
-                    FloatingActionMenuBehavior.this.mIsAnimatingIn = false;
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        mIsAnimatingIn = false;
+                    }
+                });
 
-                }
-            })
-                    .start();
-        } else {
-            Animation anim = AnimationUtils.loadAnimation(button.getContext(), android.support.design.R.anim.design_fab_in);
-            anim.setDuration(200L);
-            anim.setInterpolator(INTERPOLATOR);
-            button.startAnimation(anim);
-        }
     }
 }
